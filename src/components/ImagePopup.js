b79b185904851a598e41e04e384959e6afbeff6f
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2022-07-29 10:15:31 -0700

    add outside click to all popups

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function ImagePopup({ card, onClose }) {
+  const imageRef = useRef();
+  function handleOutsideClick(e) {
+    if (card && imageRef.current === e.target) {
+      onClose();
+    }
+  }
+
   return (
     <div
       className={`modal modal_type_image ${card ? "modal_open" : ""}`}
       id="modal-image-popup"
+      onClick={handleOutsideClick}
+      ref={imageRef}
     >
       <div className="modal__content modal__content_type_image">
         <button
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 /* ------------------------- function Popup ------------------------- */
 function Popup({ isOpen, onClose, name, children }) {
+  const popupRef = useRef();
 
-    
+  function handleModalClick(e) {
+    if (isOpen && popupRef.current === e.target) {
+      onClose();
+    }
+  }
+
   return (
-    <div className={`modal ${isOpen ? "modal_open" : ""}`} id={`modal-${name}`} onClick={onClose}>
+    <div
+      className={`modal ${isOpen ? "modal_open" : ""}`}
+      id={`modal-${name}`}
+      ref={popupRef}
+      onClick={handleModalClick}
+    >
       <div className="modal__content">{children}</div>
     </div>
   );
